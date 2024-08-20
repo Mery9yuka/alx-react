783ac25befd7b2444273926cb4b54d2d1293d653
Mery9yuka <benhaida.maryem@gmail.com>
2024-08-20 00:05:55 +0000

    webpack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,10 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
-const updateCounter = () => {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
+let conunt = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
 };
 
 $('button').on('click', _.debounce(updateCounter, 500));
